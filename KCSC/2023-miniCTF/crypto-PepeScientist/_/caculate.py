d5c6205a3778be365874d9fc0979fb8ff3f8e0be
George Do <contact.georgedo@gmail.com>
2023-09-24 18:34:20 +0700

    Delete KCSC/2023-miniCTF directory

--- KCSC/2023-miniCTF/crypto-PepeScientist/_/caculate.py
+++
@@ -1,4 +0,0 @@
-def tinh_khang_the(khang_the_bay_gio):
-    khang_the = khang_the_bay_gio*(1+r)^N
-    return khang_the
-
--- KCSC/2023-miniCTF/crypto-PepeScientist/solve.py
+++
@@ -1,9 +0,0 @@
-x = 100000
-y = 1000000
-
-for i in range(10000): 
-    x *= (1.68/100 + 1)
-    print(i, x)
-    if (x >= y):
-        break
-
