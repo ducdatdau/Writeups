e50a70648bf705e6b9ac4236c2afee016ae5c5ec
geordo <contact.georgedo@gmail.com>
2024-05-12 23:39:59 +0700

    dreamhack basic exp 2

---
+++ Training/DreamHack/basic_exploitation_002/basic_exploitation_002.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+void alarm_handler() {
+    puts("TIME OUT");
+    exit(-1);
+}
+
+
+void initialize() {
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    signal(SIGALRM, alarm_handler);
+    alarm(30);
+}
+
+void get_shell() {
+    system("/bin/sh");
+}
+
+int main(int argc, char *argv[]) {
+
+    char buf[0x80];
+
+    initialize();
+
+    read(0, buf, 0x80);
+    printf(buf);
+
+    exit(0);
+}
